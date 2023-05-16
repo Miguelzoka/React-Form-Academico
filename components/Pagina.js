76cc326aa008f54219c30daeb173d24cc12f45e6
Miguel Lima <miguel.pwg@gmail.com>
2023-05-15 22:02:37 -0300

    primeiro commit

---
+++ components/Pagina.js
@@ -0,0 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react"
+import Cabecalho from "./Cabecalho"
+import Rodape from "./Rodape"
+import { Container } from 'react-bootstrap';
+
+const Pagina = (props) => {
+  return (
+    <>
+      
+      <Cabecalho />
+      <div className="bg-secondary text-white mb-1 py-3 text-center">
+        <h1>{props.titulo}</h1>
+      </div>
+
+      <Container className='mb-5'>
+      {props.children}
+      </Container>
+
+      <Rodape/>
+      
+    </>
+  )
+}
+
+export default Pagina
